7f2985b1d6bba9a600843509574e6c417a09a43f
NoMoreZer0 <kazakupersuper@mail.ru>
2023-05-10 13:07:17 +0600

    added healthcheck

--- src/main/java/com/example/singularitymanagement/security/SecurityConfiguration.java
+++ src/main/java/com/example/singularitymanagement/security/SecurityConfiguration.java
@@ -36,7 +36,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/register", "/login", "/h2-console/**").permitAll()
+                .antMatchers("/register", "/login", "/h2-console/**", "/healthcheck").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .cors()
